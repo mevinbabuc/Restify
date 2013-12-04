ad5e305287ecdb0e5c30fd44c58f725b0d418511
Mevin <mevinbabuc@gmail.com>
2013-12-04 21:59:23 +0530

    Restify initialized

---
+++ model.py
@@ -0,0 +1,10 @@
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+class HashStore(ndb.Model):
+    """Models an individual HashStore entry with hastag, tile, and date."""
+
+    author = ndb.UserProperty()
+    title = ndb.StringProperty(indexed=False)
+    hashtag = ndb.StringProperty(indexed=True, default="")
+    viewDate = ndb.DateTimeProperty(auto_now_add=True)
