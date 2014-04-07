614901ccb4ef0252938359d03ccce979943b0688
Mevin <mevinbabuc@gmail.com>
2014-04-07 13:15:31 +0530

    Update settings.py

--- ReSTify/settings.py
+++ ReSTify/settings.py
@@ -1,8 +1,7 @@
-
 # Add the URL of the site requesting the ReST server i.e. origin of request to the ReST server*/
 ORIGIN_SITE_NAME = "http://example.com"
 
-# Alias name for the models, If you dont want to expose you Model name or for more user friendly URL
+# Alias name for the models, If you dont want to expose you Model name or for a neat URL
 MODEL_NAME_ALIAS = {
   'hashtag': 'HashStore',
   }
