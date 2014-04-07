e5290fe6b30e2b6de9f74f778c8f879c6854dfa8
Mevin <mevinbabuc@gmail.com>
2014-04-07 13:14:57 +0530

    Added URL to model name mapping.
    Just makes your URL look neat if you have weird model names.

--- ReSTify/settings.py
+++ ReSTify/settings.py
@@ -1,1 +1,8 @@
+
+# Add the URL of the site requesting the ReST server i.e. origin of request to the ReST server*/
 ORIGIN_SITE_NAME = "http://example.com"
+
+# Alias name for the models, If you dont want to expose you Model name or for more user friendly URL
+MODEL_NAME_ALIAS = {
+  'hashtag': 'HashStore',
+  }
